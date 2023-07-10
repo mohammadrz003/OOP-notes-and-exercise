5174aecc796b42642bccf822a00495a468f277e9
mohammad rezaii <rezaiimohammad003@gmail.com>
2023-07-10 14:56:24 +0330

    Creating objects that have behvior with function factories

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const circle = {
-  radius: 10,
-  location: {
-    x: 1,
-    y: 1,
-  },
-  draw: function () {
-    console.log("draw");
-  },
-};
+// Factory Function
+function createCircle(radius) {
+  return {
+    radius,
+    draw: function () {
+      console.log("draw");
+    },
+  };
+}
 
+const circle = createCircle(10);
 circle.draw();
