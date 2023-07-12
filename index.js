c16bc3868f9093b8fd7a433f71120ededb8ccf6f
mohammad rezaii <rezaiimohammad003@gmail.com>
2023-07-12 14:09:04 +0330

    Resetting the constructor

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-function Shape() {}
+function Shape(color) {
+  this.color = color;
+}
 
 Shape.prototype.duplicate = function () {
   console.log("duplicate");
 };
 
-function Circle(radius) {
+function Circle(radius, color) {
+  Shape.call(this, color);
   this.radius = radius;
 }
 
 Circle.prototype = Object.create(Shape.prototype);
+Circle.prototype.constructor = Circle;
 
 Circle.prototype.draw = function () {
   console.log("draw");
 };
 
-let c = new Circle(1);
+let c = new Circle(1, "green");
