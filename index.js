82d959b8978fb9b2543752f09ee0c9368c1e1b16
mohammad rezaii <rezaiimohammad003@gmail.com>
2023-07-12 11:39:05 +0330

    Prototype vs Instance members

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 function Circle(radius) {
+  // Instance members
   this.radius = radius;
-
-  this.draw = function () {
-    console.log("draw");
-  };
 }
 
-const circle = new Circle(10);
+// Circle.prototype === c1.__proto__;
+
+// Prototype members
+Circle.prototype.draw = function () {
+  console.log("draw");
+};
+
+const c1 = new Circle(1);
+const c2 = new Circle(1);
+
+Circle.prototype.toString = function () {
+  return `Circle with radius ${this.radius}`;
+};
