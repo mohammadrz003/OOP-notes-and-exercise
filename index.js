939f40a6b2916fbe1dbcd420b684621356d7f77c
mohammad rezaii <rezaiimohammad003@gmail.com>
2023-07-12 14:53:05 +0330

    Method overriding

--- index.js
+++ index.js
@@ -4,31 +4,19 @@ function extend(Child, Parent) {
   Child.prototype.constructor = Child;
 }
 
-function Shape(color) {
-  this.color = color;
-}
+function Shape() {}
 
 Shape.prototype.duplicate = function () {
   console.log("duplicate");
 };
 
-function Circle(radius, color) {
-  Shape.call(this, color);
-  this.radius = radius;
-}
+function Circle() {}
 
 extend(Circle, Shape);
 
-Circle.prototype.draw = function () {
-  console.log("draw");
+Circle.prototype.duplicate = function () {
+  Shape.prototype.duplicate.call(this);
+  console.log("duplicate circle");
 };
 
-function Square(size, color) {
-  Shape.call(this, color);
-  this.size = size;
-}
-
-extend(Square, Shape);
-
 let c = new Circle(1, "green");
-let s = new Square(5, "red");
