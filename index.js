34f4a93467ee78f583cf056360bf8fed525d784a
mohammad rezaii <rezaiimohammad003@gmail.com>
2023-07-12 10:08:20 +0330

    Multilevel inheritence

--- index.js
+++ index.js
@@ -1,45 +1,9 @@
-function Stopwatch() {
-  let duration = 0;
+function Circle(radius) {
+  this.radius = radius;
 
-  let isStarted = false;
-  let isStoped = false;
-
-  let startMoment = null;
-  let endMoment = null;
-
-  this.start = function () {
-    if (isStarted === true) {
-      throw new Error("Stopwatch has already started");
-    }
-    startMoment = new Date();
-    isStarted = true;
+  this.draw = function () {
+    console.log("draw");
   };
-
-  this.stop = function () {
-    if (isStoped === true) {
-      throw new Error("Stopwatch has already stoped");
-    } else if (!isStoped && !isStarted) {
-      throw new Error("Stopwatch hasn't started yet");
-    }
-    endMoment = new Date();
-    isStoped = false;
-    isStarted = false;
-    duration += (endMoment.getTime() - startMoment.getTime()) / 1000;
-  };
-
-  this.reset = function () {
-    isStarted = false;
-    isStoped = false;
-    startMoment = null;
-    endMoment = null;
-    duration = 0;
-  };
-
-  Object.defineProperty(this, "duration", {
-    get: function () {
-      return duration;
-    },
-  });
 }
 
-let sw = new Stopwatch();
+const circle = new Circle(10);
