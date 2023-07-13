6adaf6ac897418b63ab2e4bdaef0134ac6889254
mohammad rezaii <rezaiimohammad003@gmail.com>
2023-07-13 10:25:16 +0330

    Polymorphism excercise

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ HtmlElement.prototype.focus = function () {
   console.log("focused");
 };
 
+const e = new HtmlElement();
+
 function HtmlSelectElement(items = []) {
   this.items = items;
 
@@ -21,10 +23,42 @@ function HtmlSelectElement(items = []) {
   this.removeItem = function (item) {
     this.items = this.items.filter((value) => value !== item);
   };
-}
 
-const e = new HtmlElement();
+  this.render = function () {
+    const selectElement = document.createElement("select");
+    for (let i = 0; i < this.items.length; i++) {
+      const option = document.createElement("option");
+      option.value = this.items[i];
+      option.text = this.items[i];
+      selectElement.appendChild(option);
+    }
+    return selectElement;
+  };
+}
 
 HtmlSelectElement.prototype = e;
 
+function HtmlImageElement(src) {
+  let src = src;
+
+  this.render = function () {};
+
+  Object.defineProperty(this, "src", {
+    get: function () {
+      return src;
+    },
+    set: function (value) {
+      if (typeof value !== "string") {
+        throw new Error("Value must be string");
+      }
+      src = value;
+    },
+  });
+}
+
+HtmlImageElement.prototype = e;
+HtmlImageElement.prototype.constructor = HtmlImageElement;
+
+// create instances
 const se = new HtmlSelectElement([1, 2, 3]);
+const img = new HtmlImageElement();
