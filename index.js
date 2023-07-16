cab0dadc6ef1ab4d477cc00383db3d6aef91137d
mohammad rezaii <rezaiimohammad003@gmail.com>
2023-07-16 08:52:49 +0330

    ES6 exercise: Stack class

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
-class Shape {
-  move() {
-    console.log("move");
+const _count = new WeakMap();
+const _container = new WeakMap();
+
+class Stack {
+  constructor() {
+    _count.set(this, 0);
+    _container.set(this, []);
+  }
+
+  get count() {
+    return _count.get(this);
+  }
+
+  push(value) {
+    _container.get(this).push(value);
+    _count.set(this, this.count + 1);
+  }
+
+  pop() {
+    if (_container.get(this).length === 0) throw new Error("Stack is empty");
+    const poppedProperty = _container.get(this).pop();
+    _count.set(this, this.count - 1);
+    return poppedProperty;
+  }
+
+  peek() {
+    if (_container.get(this).length === 0) throw new Error("Stack is empty");
+    const peekedProperty =
+      _container.get(this)[_container.get(this).length - 1];
+    return peekedProperty;
   }
 }
 
-class Circle extends Shape {
-  move() {
-    super.move();
-    console.log("circle move");
-  }
-}
-
-const c = new Circle();
+const s = new Stack();
